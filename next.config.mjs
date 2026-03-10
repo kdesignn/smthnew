03518581d5f9dbfed78a7816f3670a00853521f6
Kshitij <kshitijy2k@gmail.com>
2026-03-10 02:00:20 +0100

    fix missing images on gh-pages with basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
+    basePath: isProd ? '/smthnew' : '',
+    assetPrefix: isProd ? '/smthnew/' : '',
     images: {
         unoptimized: true,
     },
