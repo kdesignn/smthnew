2cc01015eb672d1e7a9a23a2eaafe510b760a2f7
Kshitij <kshitijy2k@gmail.com>
2026-03-25 16:07:18 +0100

    Simplify Next config for Pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-
 const nextConfig = {
     reactStrictMode: true,
     output: "export",
-    basePath,
-    assetPrefix: basePath ? `${basePath}/` : undefined,
-    turbopack: {
-        root: __dirname,
-    },
     images: {
         unoptimized: true,
     },
